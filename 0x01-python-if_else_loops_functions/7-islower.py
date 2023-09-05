80b2f54fa14e9ae100cdafb95f5bb891b958cc3f
Ahmed Rashad <eng.ahmadrashad@yahoo.com>
2023-09-05 15:23:16 -0700

    f

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-def islower(c) :
-    if ord(c) >= ord('a') and ord(c) <= ord('z') :
+def islower(c):
+    if ord(c) >= ord('a') and ord(c) <= ord('z'):
         return True
     else:
         return False
