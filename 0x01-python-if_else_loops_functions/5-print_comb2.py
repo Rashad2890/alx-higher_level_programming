bd495ae473c6a15d0bb28930e8926a58ef2e87fa
Ahmed Rashad <eng.ahmadrashad@yahoo.com>
2023-09-05 15:18:32 -0700

    fix

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-for l in range(100):
-    print("{:02d}".format(l), end="\n" if l == 99 else", ")
+for n in range(100):
+    print("{:02d}".format(n), end="\n" if n == 99 else", ")
