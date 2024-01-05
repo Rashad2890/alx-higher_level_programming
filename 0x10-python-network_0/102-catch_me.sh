e7399ae8cac6c76c1f4213f4d07174413c8e4a41
Ahmed Rashad <eng.ahmadrashad@yahoo.com>
2024-01-05 20:33:54 +0200

    102

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # catch me that causes the server response
-curl -s -L -H "You got me!" -X PUT -d "user_id=98" 0.0.0.0:5000/catch_me
+curl -sL -X PUT -H "Origin: HolbertonSchool" -d "user_id=98" 0.0.0.0:5000/catch_me
