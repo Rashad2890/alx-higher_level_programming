d0b29f2af6e73b71ea04f960c54c4acfa9053481
Ahmed Rashad <eng.ahmadrashad@yahoo.com>
2024-01-05 20:29:20 +0200

    6f

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,13 +1,26 @@
 #!/usr/bin/python3
-""" Test function find_peak """
+"""Defines a peak-finding algorithm."""
 
 
-def find_peak(lx):
-    """fynction to find peak """
-    if not lx:
+def find_peak(list_of_integers):
+    """ Finds the peak in a list of integers """
+    if list_of_integers == []:
         return None
-    for i in range(len(lx)):
-        if lx[0] < lx[i]:
-            lx[0] = lx[i]
 
-    return lx[0]
+    length = len(list_of_integers)
+    m = int(length / 2)
+    li = list_of_integers
+
+    if m - 1 < 0 and m + 1 >= length:
+        return li[m]
+    elif m - 1 < 0:
+        return li[m] if li[m] > li[m + 1] else li[m + 1]
+    elif m + 1 >= length:
+        return li[m] if li[m] > li[m - 1] else li[m - 1]
+
+    if li[m - 1] < li[m] > li[m + 1]:
+        return li[m]
+
+    if li[m + 1] > li[m - 1]:
+        return find_peak(li[m:])
+    return find_peak(li[:m])
