e176c2f631cad8141f2b51a4a4af1e82936b5af4
Ahmed Rashad <eng.ahmadrashad@yahoo.com>
2024-01-30 19:34:13 +0200

    aa

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,32 +1,20 @@
 #!/usr/bin/node
-
 const request = require('request');
-const url = 'http://swapi.co/api/films/' + process.argv[2];
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 request(url, function (error, response, body) {
-  if (error) {
-    console.log(error);
-  } else {
-    let data = JSON.parse(body).characters;
-    // console.log(data);
-    let order = [];
-    for (let i = 0; i < data.length; i++) {
-      order.push(new Promise(function (resolve, reject) {
-        request.get(data[i], function (error, response, body) {
-          if (error) {
-            reject(error);
-          }
-          resolve(JSON.parse(body).name);
-        });
-      }));
-      // console.log(order[i]);
-    }
-    Promise.all(order).then(character => {
-      for (let j = 0; j < character.length; j++) {
-        console.log(character[j]);
-      }
-      // can shorten above 2 lines into this 1 line below
-      // character.forEach(person => console.log(person));
-    });
-    // console.log(order);
+  if (!error) {
+    const characters = JSON.parse(body).characters;
+    printCharacters(characters, 0);
   }
 });
+
+function printCharacters (characters, index) {
+  request(characters[index], function (error, response, body) {
+    if (!error) {
+      console.log(JSON.parse(body).name);
+      if (index + 1 < characters.length) {
+        printCharacters(characters, index + 1);
+      }
+    }
+  });
+}
