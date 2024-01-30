1d78216bf49488b54687bfa68547c934b37fef33
Ahmed Rashad <eng.ahmadrashad@yahoo.com>
2024-01-30 19:17:56 +0200

    3

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,28 +1,9 @@
 #!/usr/bin/node
 
 const request = require('request');
-const argv = process.argv;
-const url = 'http://swapi.co/api/films/';
-
-function getStatusJson (theUrl) {
-  const options = {
-    url: theUrl,
-    method: 'GET',
-    headers: {
-      Accept: 'application/json',
-      'Accept-Charset': 'utf-8'
-    }
-  };
-  request(options, function (err, res, body) {
-    if (err) {
-      console.log(err);
-    } else {
-      const json = JSON.parse(body);
-      const status = res.statusCode;
-      console.log(json.title);
-      return (status, json);
-    }
-  });
-}
-
-getStatusJson(url + argv[2]);
+const id = process.argv[2];
+const url = 'https://swapi-api.hbtn.io/api/films/' + id;
+request(url, function (err, response, body) {
+  if (err) { console.error('error:', err); }
+  console.log(JSON.parse(body).title);
+});
